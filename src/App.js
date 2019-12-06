ff5e8179c4b6a7222fa67aedbcd1d846130ef580
GERALD WONG <ramza88@gmail.com>
2019-12-06 18:03:37 +0800

    shifted files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Visualizer from './Visualizer/Visualizer'
+import VisualizerComponent from './Visualizer/Visualizer';
 
 class App extends Component {
   render() {
@@ -14,7 +14,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload
 
-          <Visualizer/>
+          <VisualizerComponent/>
         </p>
       </div>
     );
--- src/Visualizer/Visualizer.jsx
+++ src/Visualizer/Visualizer.jsx
@@ -1,21 +1,85 @@
-import React, { useState } from 'react';
-import GridComponent from './Grid/Grid'
+import React, { useState, useRef, useReducer } from 'react';
+import './Visualizer.css'
+import Cell, { CellTypes } from './Cell'
 
-function Visualizer() {
+const visualizerState = {
+    grid: createGrid(20, 50),
+    isMousePressed: false,
+    mode: CellTypes.OBSTACLE
+}
 
-    const [mode, setMode] = useState(0)
+const visualizerReducer = (state, action) => {
+    function paintCell(row, col, type) {
+        let newGrid = state.grid.slice();
+        newGrid[row][col].type = type;
+        return newGrid;
+    }
 
+    switch(action.type) {
+        case "cellMouseDown":
+            return { ...state, grid: paintCell(action.row, action.col, state.mode), isMousePressed: true };
+        case "cellMouseUp":
+            return {...state, isMousePressed: false}
+        case "cellMouseEnter":
+            console.log(state)
+            if (state.isMousePressed) {
+                return { ...state, grid: paintCell(action.row, action.col, state.mode), isMousePressed: true };
+            }
+            return {...state}
+        default:
+            throw new Error('unexpected action');
+    }
+}
+
+
+
+function VisualizerComponent() {
+    const [state, dispatch] = useReducer(visualizerReducer, visualizerState);
+    
     return (
-        <div>
-            <div>
-                <button>Mode 1</button>
-                <button>Mode 2</button>
-                <button>Mode 3</button>
-            </div>
-            <GridComponent/>
+        <div id="grid" className="grid">
+            {state.grid.map((rowObj, rowId) => {
+                return ( 
+                    <div id={`row-${rowId}`} key={`row-${rowId}`}> 
+                    {rowObj.map((colObj) => {
+                        const {row, col, type} = colObj;
+                        return (
+                            <Cell 
+                                row={row} 
+                                col={col} 
+                                type={type} 
+                                dispatch={dispatch}
+                            />
+                        )
+                    })}
+                    </div>
+                )
+            })}
         </div>
     )
 }
 
+function createGrid(rows, cols) {
+    const grid = [];
+    for (let r = 0; r < rows; ++r) {
+        const currentRow = [];
+        for ( let c = 0; c < cols; ++c) {
+            currentRow.push(createNode(r, c, 0));
+        }
+        grid.push(currentRow);
+    }
+    return grid;
+}
 
-export default Visualizer;
+
+function createNode(row, col, type) {
+    return {
+        row, 
+        col,
+        type
+    };
+}
+
+
+
+export default VisualizerComponent;
